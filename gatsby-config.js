e6f1f5d6d28f9c670d730c6604aa4af0754edfd2
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-11-19 13:56:16 +0100

    feat: install as a dependency prism.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,27 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
-  },
-  plugins: [
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      },
+    siteMetadata: {
+        title: `Gatsby Default Starter`,
+        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        author: `@gatsbyjs`
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
-  ]
+    plugins: [
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`
+            }
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                showLineNumbers: true
+            }
+        }
+    ]
 }
