dc71fd59b23d76893bde3770ba4795ce55eaa317
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-12-03 10:32:03 +0100

    fix: update <Snippet/>

--- src/components/Snippet/Snippet.tsx
+++ src/components/Snippet/Snippet.tsx
@@ -1,102 +1,108 @@
-import React from "react"
-import PropTypes from "prop-types"
 import Prism from "prismjs"
-
-
+import PropTypes from "prop-types"
+import React from "react"
 
 
 export interface Props {
-  name: string
-  since: null | string
-  description: null | string
-  examples: string[]
-  signature: {
-    typeparameters: {
-      name: null | string
-      description: null | string
-      defaultValue: null | string
-      extends: null | string
-    }[]
-    parameters: {
-      name: string
-      type: null | string
-      description: null | string
-      defaultValue: null | string
-    }[]
-    returnValue: {
-      type: null | string
-      description: null | string
+    name: string
+    since: null | string
+    description: null | string
+    examples: string[]
+    signature: {
+        typeparameters: {
+            name: null | string
+            description: null | string
+            defaultValue: null | string
+            extends: null | string
+        }[]
+        parameters: {
+            name: string
+            type: null | string
+            description: null | string
+            defaultValue: null | string
+        }[]
+        returnValue: {
+            type: null | string
+            description: null | string
+        }
+    },
+    metadata: {
+        source: null | string,
+        specification: null | string
+    },
+    pkg: {
+        name: string,
+        version: string
     }
-  },
-  metadata: {
-    source: null | string,
-    specification: null | string
-  },
-  pkg: {
-    name: string,
-    version: string
-  }
 }
 
 Snippet.propTypes = {
-  name: PropTypes.string.isRequired,
-  since: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  signature: PropTypes.object.isRequired,
-  examples: PropTypes.arrayOf(PropTypes.string).isRequired,
-  metadata: PropTypes.object.isRequired,
-  pkg: PropTypes.object.isRequired
+    name: PropTypes.string.isRequired,
+    since: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    signature: PropTypes.object.isRequired,
+    examples: PropTypes.arrayOf(PropTypes.string).isRequired,
+    metadata: PropTypes.object.isRequired,
+    pkg: PropTypes.object.isRequired
 }
 
 
 function Snippet({ name, since, description, signature, examples, metadata, pkg }: Props) {
-  return (
-    <div id={name} className="card snippet">
-      <div className="header">
-        <div className="title">{name}</div>
-      </div>
-      <div className="content">
-        <p>{description}</p>
-      </div>
-      <div className="collapsible">
-        <div className="content">
-          <h3 className="header">Parameters</h3>
-          <table>
-            <tbody>
-              {signature.parameters.map((parameter) =>
-                <tr key={parameter.name}>
-                  <td><code><strong>{parameter.name}: {parameter.type}</strong></code></td>
-                  <td><small>{parameter.description}</small></td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-          <p><small><strong>Returns</strong> ({signature.returnValue.type}) {signature.returnValue.description}</small></p>
+    return (
+        <div id={name} className="card snippet">
+            <div className="header">
+                <div className="title">{name}</div>
+            </div>
+            <div className="content">
+                <p>{description}</p>
+            </div>
+            <div className="collapsible">
+                <div className="content">
+                    <h3 className="header">Parameters</h3>
+                    <table>
+                        <tbody>
+                        {signature.parameters.map((parameter) => {
+                                const [url, name] = (parameter.description || "")
+                                    .replace(/\{@link (.*)\}/, (g1, g2) => g2)
+                                    .split(" | ")
 
-          <h3 className="header">Metadata</h3>
-          <table>
-            <tbody>
-              <tr>
-                <td><small><strong>version</strong></small></td>
-                <td><small>v{pkg.version}</small></td>
-              </tr>
-              <tr>
-                <td><small><strong>since</strong></small></td>
-                <td><small>v{since}</small></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      {examples.map((example) =>
-          <pre key={example} className="example language-javascript">
+                                return <tr key={parameter.name}>
+                                    <td><code><strong>{parameter.name}: {parameter.type}</strong></code></td>
+                                    <td>
+                                        <small>
+                                            <a href={url}>{name}</a>
+                                        </small>
+                                    </td>
+                                </tr>
+                            }
+                        )}
+                        </tbody>
+                    </table>
+                    <p><small><strong>Returns</strong> ({signature.returnValue.type}) {signature.returnValue.description}</small></p>
+
+                    <h3 className="header">Metadata</h3>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td><small><strong>version</strong></small></td>
+                            <td><small>v{pkg.version}</small></td>
+                        </tr>
+                        <tr>
+                            <td><small><strong>since</strong></small></td>
+                            <td><small>v{since}</small></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            {examples.map((example) =>
+                    <pre key={example} className="example language-javascript">
             <code dangerouslySetInnerHTML={{ __html: Prism.highlight(example, Prism.languages.javascript, "javascript") }}/>
           </pre>
-      )}
-    </div>
-  )
+            )}
+        </div>
+    )
 }
-
 
 
 export default Snippet
