411078e5a7a3faf5ec654d18f1001f1b248d1b0b
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-12-03 10:02:30 +0100

    fix: update <Snippet/>

--- src/pages/api.tsx
+++ src/pages/api.tsx
@@ -3,8 +3,8 @@ import Drawer from "../components/Drawer/Drawer"
 import Section from "../components/Drawer/Section/Section"
 import Layout from "../components/Layout/Layout"
 import Snippet from "../components/Snippet/Snippet"
-import members from "../data/api/reflect/members.json"
-import pkg from "../data/api/reflect/package.json"
+import reflectMembers from "../data/api/reflect/members.json"
+import reflectPkg from "../data/api/reflect/package.json"
 
 
 
@@ -14,19 +14,15 @@ export default () => {
             <div className="row">
                 <Drawer>
                     <Section
-                        name="@commonly/function"
-                        items={
-                            [
-                                "identity"
-                            ]
-                        }
+                        name={reflectPkg.name}
+                        items={reflectMembers.map(member => member.name)}
                     />
                 </Drawer>
                 <div className="column">
                     <div className="container">
-                        <h1 className="header">{pkg.name}</h1>
-                        <p>description</p>
-                        {members.map((member) =>
+                        <h1 className="header">{reflectPkg.name}</h1>
+                        <p>{reflectPkg.name}</p>
+                        {reflectMembers.map((member) =>
                             <Snippet
                                 key={member.name}
                                 name={member.name}
@@ -35,7 +31,7 @@ export default () => {
                                 signature={member.signature}
                                 examples={member.examples}
                                 metadata={member.metadata}
-                                pkg={pkg}
+                                pkg={reflectPkg}
                             />
                         )}
                     </div>
