f667e2bfb1dd847face3de35c723539c51af971a
Mateusz Pietrzak <mateusz.pietrzak@tellyo.com>
2019-12-04 15:02:50 +0100

    fix: update <Snippet/>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "../components/Layout/Layout"
 
 
 
-export default () =>
-    <Layout>
+export default () => {
+    useEffect(() => {
+        window.location.pathname = "/api/"
+    }, [])
+
+    return <Layout>
         <div className="layout">
             <div className="row">
                 <div className="column">
                     <div className="container">
-                        <h1>Home!</h1>
+                        <h1></h1>
                     </div>
                 </div>
             </div>
         </div>
     </Layout>
+}
