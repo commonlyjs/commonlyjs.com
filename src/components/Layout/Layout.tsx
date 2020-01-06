77a4b5b0beb91d09a8693796545e2d04a516b7c3
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-01-06 17:44:23 +0100

    chore: add analytics

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -26,6 +26,15 @@ function Layout({ children }: Props) {
         title={"Commonly - A modern utility library for JavaScript/TypeScript."}
       >
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109513724-2"/>
+          <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-109513724-2');
+          </script>
+
       </Helmet>
       <Navbar/>
       {children}
